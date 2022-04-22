6b23a4449ce1f86d6a29a32aaaa33328dc979bec
William Rodrigues <williamrodrigues2442@gmail.com>
2022-04-22 16:10:05 -0300

    feat: add basic authentication on API

--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, filters
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
 from clients.serializers import ClientSerializer
@@ -17,3 +19,5 @@ class ClientViewSet(viewsets.ModelViewSet):
     filterset_fields = ['is_active']
     ordering_fields = ['name']
     search_fields = ['name', 'cpf']
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
